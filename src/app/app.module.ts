bcfd09fb9db2732373d32b9daa07189bef6f006b
Amanda Rovey <155773970+AR15NE@users.noreply.github.com>
2024-12-21 21:10:19 -0600

    Debugging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ResumeComponent } from './resume/resume.component';
@@ -9,26 +10,30 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'resume', component: ResumeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotFoundComponent }
+];
+
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: LandingPageComponent },
-      { path: 'resume', component: ResumeComponent },
-      { path: 'about', component: AboutComponent },
-      { path: 'projects', component: ProjectsComponent },
-      { path: 'contact', component: ContactComponent },
-      { path: '**', component: NotFoundComponent }  // Route to NotFoundComponent 404 page
-    ]),
-    AppComponent,          // Import standalone component
+    RouterModule.forRoot(routes, { useHash: true }),
+    AppComponent,
     LandingPageComponent,
     ResumeComponent,
     AboutComponent,
     ProjectsComponent,
-    ContactComponent,
+    ContactComponent,     
     NotFoundComponent
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
